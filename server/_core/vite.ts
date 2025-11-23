f98741c59b0bd6e2db3368fd1950eff4ba249b64
nonexistent99 <oniinchansensei@gmail.com>
2025-11-23 20:16:21 -0300

    Fix: remover vite completamente

--- server/_core/vite.ts
+++ server/_core/vite.ts
@@ -1,10 +1,7 @@
 import express, { type Express } from "express";
 import fs from "fs";
-import { type Server } from "http";
-import { nanoid } from "nanoid";
 import path from "path";
-import { createServer as createViteServer } from "vite";
-import viteConfig from "../../vite.config";
+
 
 export async function setupVite(app: Express, server: Server) {
   const serverOptions = {
